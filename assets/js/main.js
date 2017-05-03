7735c102e3c3bd9cefa1651f0b73a5ff20e680a9
John Pham <johnphammail@gmail.com>
2017-05-03 15:18:53 -0700

    ready for challenge

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,3 +155,16 @@
 	});
 
 })(jQuery);
+
+function showDiv() {
+    div = document.getElementById('team-display');
+		title = document.getElementById('team-button');
+		if (div.style.display == "block") {
+			div.style.display = "none";
+			title.innerHTML = "Show";
+		}
+		else {
+			div.style.display = "block";
+			title.innerHTML = "Hide";
+		}
+}
